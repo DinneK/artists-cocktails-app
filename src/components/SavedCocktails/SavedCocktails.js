7b67d80bcbeb239a0e820938272de2d333bb8c69
Dinne Kopelevich <dinne22@hotmail.com>
2022-11-11 18:06:58 -0700

    Fix search clearing

---
+++ src/components/SavedCocktails/SavedCocktails.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./SavedCocktails.css";
+
+const SavedCocktails = ({ savedCocktails }) => {
+  console.log({ savedCocktails });
+  return <h2>Hello</h2>;
+};
+
+export default SavedCocktails;
