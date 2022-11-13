7be93ce837d6b5b7dcdfffdaa8b6e518d3bc75f3
Dinne Kopelevich <dinne22@hotmail.com>
2022-11-12 17:19:56 -0700

    Styling for single cocktail

--- src/components/CocktailContainer/CocktailContainer.js
+++ src/components/CocktailContainer/CocktailContainer.js
@@ -19,37 +19,63 @@ const CocktailContainer = ({
     onDeleteCocktail(id);
   };
   return (
-    <section>
-      <div>
-        <img
-          src={cocktail.cocktail_image}
-          alt={`of ${cocktail.cocktail_name}`}
-          className="cocktail-picture"
-        />
+    <section className="single-cocktail-container">
+      <div className="inner-single-cocktail-container">
+        <div className="outer-cocktail-img-container">
+          <div className="inner-cocktail-img-container">
+            <img
+              src={cocktail.cocktail_image}
+              alt={`of ${cocktail.cocktail_name}`}
+              className="single-cocktail-picture"
+            />
+          </div>
+        </div>
+        <div className="cocktail-description">
+          <div className="inner-cocktail-description">
+            <h2 className="single-cocktail-title">{cocktail.cocktail_name}</h2>
+            <h3 className="single-cocktail-liquor">
+              Main Liquor: {cocktail.main_cocktail_liqour}
+            </h3>
+            <p className="single-cocktail-desc">
+              Ingredients: {cocktail.ingredients}
+            </p>
+            <p className="single-cocktail-desc">
+              Instructions: {cocktail.instructions}
+            </p>
+            <p className="single-cocktail-desc">
+              {cocktail.artist_liquor_fact}
+            </p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h2>{cocktail.cocktail_name}</h2>
-        <h3>Main Liquor: {cocktail.main_cocktail_liqour}</h3>
-        <p>Ingredients: {cocktail.ingredients}</p>
-        <p>Instructions: {cocktail.instructions}</p>
-        <p>{cocktail.artist_liquor_fact}</p>
+      <div className="cocktail-button-styling">
+        <Link to={`/artists/${cocktail.id}`}>
+          <button className="back">BACK</button>
+        </Link>
+        <Link to={`/`}>
+          <button className="home-from-cocktail">HOME</button>
+        </Link>
+        {!savedCocktails.includes(cocktail.id) && (
+          <button
+            className="save"
+            onClick={() => handleSaveCocktailClick(cocktail.id)}
+          >
+            <img className="save-logo" src={bookmarkAdd} alt="add favorite" />
+          </button>
+        )}
+        {savedCocktails.includes(cocktail.id) && (
+          <button
+            className="save"
+            onClick={() => handleDeleteCocktailClick(cocktail.id)}
+          >
+            <img
+              className="save-logo"
+              src={bookmarkDelete}
+              alt="delete favorite"
+            />
+          </button>
+        )}
       </div>
-      <Link to={`/artists/${cocktail.id}`}>
-        <button>PRESS ME! I'll take you back to you work of art </button>
-      </Link>
-      <Link to={`/`}>
-        <button>PRESS ME! I'll take you home</button>
-      </Link>
-      {!savedCocktails.includes(cocktail.id) && (
-        <button onClick={() => handleSaveCocktailClick(cocktail.id)}>
-          <img src={bookmarkAdd} alt="add favorite" />
-        </button>
-      )}
-      {savedCocktails.includes(cocktail.id) && (
-        <button onClick={() => handleDeleteCocktailClick(cocktail.id)}>
-          <img src={bookmarkDelete} alt="delete favorite" />
-        </button>
-      )}
     </section>
   );
 };
